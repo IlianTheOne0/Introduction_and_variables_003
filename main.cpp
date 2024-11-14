d54a663d92e64a82726b714a2dcfd3e08c779170
Ilian Shchepinskyi <iliantheone@gmail.com>
2024-11-15 00:06:28 +0200

    Completed Task 2

--- main.cpp
+++ main.cpp
@@ -7,6 +7,26 @@ using std::cin;
 
 int main()
 {
+	float pi = 3.14; // I could have used the math.h library, but the task specifies exactly this number
+
+	short int length;
+
+	cout << "Enter the length of the circle: ";
+	cin >> length;
+	
+	cout << endl;
+
+	cout << "Formulas" << endl;
+	cout << "|-- L = 2 * pi * R" << endl;
+	cout << "| ==> R = L / 2 * pi" << endl;
+	cout << "| ==> S = pi * R^2" << endl;
+
+	cout << endl;
+
+	float radius = length / (2 * pi);
+	float square = pi * pow(radius, 2);
+	
+	cout << "S = " << square << endl;
 
 	_getch();
 	return 0;
