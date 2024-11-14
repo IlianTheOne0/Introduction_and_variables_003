9f4f52216430eb3a498073be6c21f887218a5c71
Ilian Shchepinskyi <iliantheone@gmail.com>
2024-11-15 00:07:18 +0200

    Completed Task 1

--- main.cpp
+++ main.cpp
@@ -14,12 +14,12 @@ int main()
 	short int R3 = input(3);
 
 	float first_action = (1.0 / R1) + (1.0 / R2) + (1.0 / R3);
-	float result = 1 / first_action;
+	float R0 = 1 / first_action;
 
 	cout << endl;
 
 	cout << "Formula: (1 / R0) = (1 / R1) + (1 / R2) + (1 / R3)" << endl;
-	cout << "(1 / R0) = " << result << endl;
+	cout << "R0 = " << R0 << endl;
 
 	_getch();
 	return 0;
