1363116e43e0c14bfff646a994d07f5697c27311
Ilian Shchepinskyi <iliantheone@gmail.com>
2024-11-15 00:24:50 +0200

    Completed Task 3

--- main.cpp
+++ main.cpp
@@ -7,6 +7,25 @@ using std::cin;
 
 int main()
 {
+	short int speed;
+	short int time;
+	short int acceleration;
+
+	cout << "Enter the speed: ";
+	cin >> speed;
+
+	cout << "Enter the time: ";
+	cin >> time;
+	
+	cout << "Enter the acceleration: ";
+	cin >> acceleration;
+
+	cout << endl;
+
+	float traveled_distance = (speed * time) + ((acceleration * pow(time, 2)) / 2);
+
+	cout << "Formula: S = (v * t) + ((a * t^2) / 2)" << endl;
+	cout << "S = " << traveled_distance << "m" << endl;
 
 	_getch();
 	return 0;
