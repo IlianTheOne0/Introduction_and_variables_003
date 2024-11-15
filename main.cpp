e2ff773021b9e878aa0ccf1bfcaa1c3c2aa7d41d
Ilian Shchepinskyi <iliantheone@gmail.com>
2024-11-15 22:54:35 +0200

    Completed Task 1

--- main.cpp
+++ main.cpp
@@ -1,12 +1,33 @@
 #include <iostream>
 #include <conio.h>
+#include <Windows.h>
+
+HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 
 using std::cout;
 using std::endl;
 using std::cin;
+using std::string;
 
 int main()
 {
+	const char* name = "The war and the peace";
+	const char* author = "L.N. Tolstoj";
+	const char* publisher = "Piter";
+	short int pages = 500;
+
+	cout << "\t\tName\t   : ";
+	SetConsoleTextAttribute(hConsole, 6);
+	cout << "\"" << name << "\"" << endl;
+	SetConsoleTextAttribute(hConsole, 7);
+
+	cout << "\t\tAvtor\t   :   " << author << endl;
+
+	cout << "\t\tIzdatelstvo:  " << publisher << endl;
+
+	cout << "\t\tPages\t   : ";
+	SetConsoleTextAttribute(hConsole, 5);
+	cout << pages << endl;
 
 	_getch();
 	return 0;
