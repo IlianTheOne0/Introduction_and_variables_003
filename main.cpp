a8a9fe54292e4e505272252ac0e37ff1f3a94eda
Ilian Shchepinskyi <iliantheone@gmail.com>
2024-11-15 22:59:02 +0200

    Completed Task 2

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <conio.h>
+#include <Windows.h>
+
+HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 
 using std::cout;
 using std::endl;
@@ -7,6 +10,14 @@ using std::cin;
 
 int main()
 {
+	SetConsoleTextAttribute(hConsole, 6);
+
+	cout << "\"U lukomor'a dub zelenij," << endl;
+	cout << " Zlataya zep na dupe tom," << endl;
+	cout << " I dnem i nochju kot uchenij" << endl;
+	cout << " vse hodit po cepi krugom\"" << endl;
+
+	SetConsoleTextAttribute(hConsole, 7);
 
 	_getch();
 	return 0;
