a767e66a2bd908fdfeb2fa809e7fec3b6d75a026
Ilian Shchepinskyi <iliantheone@gmail.com>
2024-11-15 23:28:27 +0200

    Completed Task 3

--- main.cpp
+++ main.cpp
@@ -7,6 +7,144 @@ using std::cin;
 
 int main()
 {
+	cout << char(201);
+
+	for (short int i = 0; i <= 98; i++)
+	{
+		cout << (char)205;
+	} 
+
+	cout << (char)187 << endl;
+	
+
+
+	cout << (char)186;
+
+	for (short int i = 0; i <= ((97 / 2) - (12 / 2)); i++)
+		cout << " ";
+	cout << "Vremena goda";
+	for (short int i = 0; i <= ((98 / 2) - (12 / 2)); i++)
+		cout << " ";
+
+	cout << (char)186 << endl;
+
+
+
+	cout << (char)204;
+
+	for (short int i = 0; i <= ((98 - 5) / 4); i++)
+	{
+		cout << (char)205;
+	}
+
+	cout << (char)203;
+
+	for (short int i = 0; i <= ((98 - 5) / 4); i++)
+	{
+		cout << (char)205;
+	}
+
+	cout << (char)203;
+
+	for (short int i = 0; i <= ((98 - 5) / 4); i++)
+	{
+		cout << (char)205;
+	}
+
+	cout << (char)203;
+
+	for (short int i = 0; i <= ((98 - 5) / 4); i++)
+	{
+		cout << (char)205;
+	}
+
+	cout << (char)185 << endl;
+
+
+
+	cout << (char)186;
+
+	for (short int i = 0; i < (((((98 - 5) / 4) - 4) / 2) + 1); i++)
+	{
+		cout << " ";
+	}
+	cout << "Zima";
+	for (short int i = 0; i < (((((98 - 5) / 4) - 4) / 2) + 1); i++)
+	{
+		cout << " ";
+	}
+
+	cout << (char)186;
+
+	for (short int i = 0; i < (((((98 - 5) / 4) - 5) / 2) + 1); i++)
+	{
+		cout << " ";
+	}
+	cout << "Vesna";
+	for (short int i = 0; i < (((((98 - 5) / 4) - 5) / 2)); i++)
+	{
+		cout << " ";
+	}
+
+	cout << (char)186;
+
+	for (short int i = 0; i < (((((98 - 5) / 4) - 4) / 2) + 1); i++)
+	{
+		cout << " ";
+	}
+	cout << "Leto";
+	for (short int i = 0; i < (((((98 - 5) / 4) - 4) / 2) + 1); i++)
+	{
+		cout << " ";
+	}
+
+	cout << (char)186;
+
+	for (short int i = 0; i < (((((98 - 5) / 4) - 4) / 2) + 1); i++)
+	{
+		cout << " ";
+	}
+	cout << "Osen";
+	for (short int i = 0; i < (((((98 - 5) / 4) - 4) / 2) + 1); i++)
+	{
+		cout << " ";
+	}
+
+	cout << (char)186 << endl;
+
+
+
+	cout << (char)200;
+
+	for (short int i = 0; i <= ((98 - 5) / 4); i++)
+	{
+		cout << (char)205;
+	}
+
+	cout << (char)202;
+
+	for (short int i = 0; i <= ((98 - 5) / 4); i++)
+	{
+		cout << (char)205;
+	}
+
+	cout << (char)202;
+
+	for (short int i = 0; i <= ((98 - 5) / 4); i++)
+	{
+		cout << (char)205;
+	}
+
+	cout << (char)202;
+
+	for (short int i = 0; i <= ((98 - 5) / 4); i++)
+	{
+		cout << (char)205;
+	}
+
+	cout << (char)188 << endl;
+
+
 
 	_getch();
 	return 0;
